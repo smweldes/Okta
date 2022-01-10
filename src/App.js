ae60b97d17a6f4d7d4a33372eb6214e5608b2a64
smweldes <weldesiem@gmail.com>
2022-01-10 12:58:29 -0800

    add sidebarApi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Integration from './components/Integration';
 
 function App() {
+  fetch('https://ct012022-oktainc.pantheonsite.io/oktaapi/integration/filters/products').then(response=>console.log(response))
   return (
     <div className="App">
       <Integration/>
