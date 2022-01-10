b9dfce7c2d2f6c72918517d41a3b65a3ec6991b6
smweldes <weldesiem@gmail.com>
2022-01-10 11:08:41 -0800

    add initial Ingegration component

---
+++ src/components/Integration.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './main.css';
+
+function Integration() {
+    return (
+        <div className='integration'>
+            
+            <div className='left'>
+                <h4>Categories</h4>
+              
+            </div>
+            <div className='right'>
+             <h3>App</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Integration
